67a93489fb48c96d2fe76af960d69c874e828b57
obay123 <obay.kadour999@yahoo.com>
2024-10-25 21:36:05 +0300

    fixing routes controllers

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,11 @@ app.use('/api/reports', reportRoutes);
 // Use sold item routes
 app.use('/api/solditems', soldItemRoutes);
 
+app.get('/',(req,res)=>{
+  res.send('hello there')
+  console.log('user hit the resources')
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
